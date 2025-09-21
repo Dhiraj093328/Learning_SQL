df1b513c89bf0296a6af4c7877f5c13e6fa8c5fb
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2025-09-21 21:47:05 +0530

    Create create and drop database.sql

---
+++ create and drop database.sql
@@ -0,0 +1,20 @@
+create database test_1;
+drop database test_1;
+
+create database dhiraj;
+drop database Dhiraj;
+
+create database sanket;
+drop database sanket;
+
+create database word;
+drop database word;
+
+create database link;
+drop database link;
+
+create database circle;
+drop database circle;
+
+create database square;
+drop database square;
