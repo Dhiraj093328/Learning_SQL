6561ce9a8d49d3a3fbdb38c1e3ff89eb2b6dc568
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2025-09-24 21:32:08 +0530

    Create create table using datatypes.sql

---
+++ create table using datatypes.sql
@@ -0,0 +1,36 @@
+
+use test1;
+-- create table using datatypes --
+create table emp(
+e_id int,
+F_name varchar(20),
+L_name varchar(20),
+salary float,
+address varchar(20),
+joining_date date,
+age tinyint,
+birth_year year,
+mobile_no varchar(30),
+gender enum('Male','FeMale','Cannot say'),
+project set('P-1','P-2','P-3','P-4')
+);
+
+-- insert information in table--
+insert into emp (e_id, F_name, L_name, salary, address, joining_date, age, birth_year, mobile_no, gender, project)
+values (101, 'Dhiraj', 'Waghmare', 50000, 'Pune', '2020-01-16', 21, 2004, '9876543210', 'Male', 'P-1');
+
+insert into emp (e_id, F_name, L_name, salary, address, joining_date, age, birth_year, mobile_no, gender, project)
+values (102, 'Karan', 'Gholap', 60000, 'Pune', '2020-01-17', 22, 2003, '7452693210', 'Male', 'P-3');
+
+insert into emp (e_id, F_name, gender, project)
+values (103, 'Sneha','FeMale', 'P-1,P-4');
+
+insert into emp (e_id, F_name, L_name, salary, address, age, gender, project)
+values (104, 'pankaja', 'Tiwari', 70000, 'Mumbai', 25,'FeMale', 'P-1');
+
+insert into emp (e_id, F_name, L_name, address,  age, mobile_no, gender, project)
+values (105, 'Akshaya', 'iyyer', 'Banglore',  39, '7850246155', 'FeMale', 'P-1,P-3');
+
+
+
+select * from emp;
